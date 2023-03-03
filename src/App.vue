b0d133a5c3122ff8dbfd38fdcaabd18aa67ab6c4
VincenzoTavassi <info@egadivacanze.it>
2023-03-03 11:48:45 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppCard from "./components/AppCard.vue";
+
+export default {
+  components: { AppHeader, AppCard },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <main></main>
+</template>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/boostrap.min.css";
+
+createApp(App).mount("#app");
