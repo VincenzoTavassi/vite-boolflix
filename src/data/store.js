964296e2ab6bb908f6c8faf997b8b6b868750bc3
VincenzoTavassi <info@egadivacanze.it>
2023-03-06 14:36:13 +0100

    app diagram

--- src/data/store.js
+++ src/data/store.js
@@ -2,7 +2,6 @@ import { reactive } from "vue";
 export const store = reactive({
   movies: [],
   series: [],
-  attori: [],
   baseUri: "https://api.themoviedb.org/3/",
   apiKey: "90ab9452368e98b81eef313c0c7bf8b0",
   isLoading: false,
